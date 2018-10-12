8323b3b0caf5f2d1c63ed7fda66645a3293b7ec7
Wei Xiang <ker_weixiang@hotmail.com>
2018-10-12 12:01:00 +0800

    Added login page

--- team-7/app/__init__.py
+++ team-7/app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+# from flask_login import LoginManager
+# from flask_mongoalchemy  import MongoAlchemy
+# from config import Config
 
 app = Flask(__name__)
+# app.config.from_object(Config)
+# db = MongoAlchemy(app)
+# login = LoginManager(app)
 
 from app import routes
--- team-7/app/routes.py
+++ team-7/app/routes.py
@@ -1,6 +1,27 @@
 from app import app
+# from .models import *
+# from flask_login import current_user, login_user
+from flask import render_template, flash, redirect, url_for, request
 
 @app.route('/')
 @app.route('/index')
 def index():
+    # if not current_user.is_authenticated:
+    #     return redirect(url_for('login'))
     return "Hello, World!"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('index'))
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    # user = User.query.filter_by(username=username).first()
+    # if user is None or not user.check_password(password):
+    #     flash('Invalid username or password')
+    #     return redirect(url_for('login'))
+    # login_user(user, remember=form.remember_me.data)
+    return redirect(url_for('index'))
+
